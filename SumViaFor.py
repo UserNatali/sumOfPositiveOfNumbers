f42a9f016e2ae71ebcfe9bbe8bac918822cd9c95
natali <kovalnatali29@gmail.com>
2022-05-26 17:14:21 +0300

    Rename file Add function len()

---
+++ SumViaFor.py
@@ -0,0 +1,8 @@
+my_list = [7, 5, 4, 4, 2, 1, -5, -10, -15]
+sum = 0
+i = 0
+for element in my_list:
+    if my_list[i] > 0:
+        sum += my_list[i]
+
+print(sum)
---
+++ SumViaWhile.py
@@ -0,0 +1,26 @@
+from typing import List
+
+my_list = [7, 5, 4, 4, 2, 1, -5, -10, -15]
+i = 0
+suma = 0
+while my_list[i] > 0:
+    suma += my_list[i]
+    i += 1
+
+print(suma)
+
+my_list2 = [7, 5, 4, 4, 2, 1]
+
+i2 = 0
+suma2 = 0
+l = len(my_list2)
+while i2 < l and my_list2[i2] > 0:
+    suma2 += my_list2[i2]
+    i2 += 1
+
+print(suma2)
+
+
+
+
+
