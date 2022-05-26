bc305b789c38efe4d0644943c4cc224fce38ce82
natali <kovalnatali29@gmail.com>
2022-05-26 22:51:24 +0300

    Reverse list

--- SumViaFor.py
+++ SumViaFor.py
@@ -1,8 +1,10 @@
 my_list = [7, 5, 4, 4, 2, 1, -5, -10, -15]
 sum = 0
-i = 0
-for element in my_list:
-    if my_list[i] > 0:
-        sum += my_list[i]
-
+l = len(my_list)
+x = range(l - 1, -1, -1)
+for index in x:
+    if my_list[index] < 0:
+        sum += my_list[index]
+    else:
+        break
 print(sum)
--- SumViaWhile.py
+++ SumViaWhile.py
@@ -19,6 +19,17 @@ while i2 < l and my_list2[i2] > 0:
     i2 += 1
 
 print(suma2)
+print(l)
+
+suma = 0
+l = len(my_list)
+i = l-1
+
+while i > 0 and my_list[i] < 0:
+    suma += my_list[i]
+    i-= 1
+
+print(suma)
 
 
 
